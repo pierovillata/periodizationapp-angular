f2c606bb097295b1983758baa6fe921bf8bd922f
pierovillata <ironpiero1@gmail.com>
2020-12-22 11:04:17 -0300

    generado componente deportes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeportesComponent } from './deportes/deportes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeportesComponent
   ],
   imports: [
     BrowserModule,
